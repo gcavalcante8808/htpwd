e3856f8ed5013e3331a6b6ad543c21b95f63748c
Gabriel Abdalla Cavalcante <gabriel.silva@serpro.gov.br>
2014-05-06 16:01:36 -0300

    Requirements now can be read by pip installations..

--- setup.py
+++ setup.py
@@ -1,12 +1,18 @@
+import os
 from distutils.core import setup
+
+with open('requirements.txt') as file:
+    required = file.read().splitlines()
 
 setup(
     name='htpwd',
     version='0.1.0',
-    packages=[''],
+    packages=['htpwd'],
     url='https://github.com/gcavalcante8808/htpwd',
     license='License :: OSI Approved :: Apache Software License',
     author='Gabriel Abdalla Cavalcante',
     author_email='gabriel.cavalcante88@gmail.com',
-    description='A web Interface for users change their own passwords on a htpasswd file.'
+    description="""A web Interface for users change their own passwords on a
+    htpasswd file.""",
+    install_requires=required,
 )
