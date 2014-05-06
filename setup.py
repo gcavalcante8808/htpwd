ac2ce825c5d13a48811302a74e0a682ac9c7710f
Gabriel Abdalla Cavalcante <gabriel.silva@serpro.gov.br>
2014-05-06 16:43:10 -0300

    Setup now uses setuptools and includes data files correctly.

--- setup.py
+++ setup.py
@@ -1,17 +1,18 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 with open('requirements.txt') as file:
     required = file.read().splitlines()
 
 setup(
     name='htpwd',
-    version='0.1.0',
-    packages=['htpwd'],
+    version='0.1.1',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=required,
     url='https://github.com/gcavalcante8808/htpwd',
     license='License :: OSI Approved :: Apache Software License',
     author='Gabriel Abdalla Cavalcante',
     author_email='gabriel.cavalcante88@gmail.com',
     description="""A web Interface for users change their own passwords on a
     htpasswd file.""",
-    install_requires=required,
 )
