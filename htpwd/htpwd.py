1597b3d4b8acda315aef097e1c00c72d847b76e1
Gabriel Abdalla Cavalcante <gabriel.silva@serpro.gov.br>
2014-05-06 19:18:23 -0300

    [A-z0-9_.]+ is the default regexp for username from now.

--- htpwd/htpwd.py
+++ htpwd/htpwd.py
@@ -13,7 +13,7 @@ import os
 # Some Usefull information come from environment. All are needed.
 HTPASSWD_FILE = os.environ.get('HTPASSWD_FILE')
 SECRET_KEY = os.environ.get('SECRET_KEY')
-REGEXP = os.environ.get('REGEXP')
+REGEXP = os.environ.get('REGEXP', "[A-z0-9_.]+")
 TARGET_PAGE = os.environ.get('TARGET_PAGE')
 
 app = Flask(__name__)
