7881e189436197928cf2e38f0c9a05b0e77917cd
Gabriel Abdalla Cavalcante <gabriel.silva@serpro.gov.br>
2014-05-06 16:51:11 -0300

    Classifiers added.

--- setup.py
+++ setup.py
@@ -15,4 +15,16 @@ setup(
     author_email='gabriel.cavalcante88@gmail.com',
     description="""A web Interface for users change their own passwords on a
     htpasswd file.""",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+	"Environment :: Web Environment",
+	"Framework :: Flask",
+	"Intended Audience :: Information Technology",
+	"Intended Audience :: System Administrators",
+	"Intended Audience :: End Users/Desktop",
+	"License :: OSI Approved :: Apache Software License",
+	"Programming Language :: Python",
+	"Programming Language :: Python :: 3",
+	"Programming Language :: Python :: 2",
+    ],
 )
