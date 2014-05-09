c44c8c8a5208a82cf7b4d4fa9805386d015e6078
Gabriel Abdalla Cavalcante <gabriel.silva@serpro.gov.br>
2014-05-09 11:36:50 -0300

    The changed page cant be reached right now.

--- htpwd/tests/functional_tests.py
+++ htpwd/tests/functional_tests.py
@@ -1,6 +1,7 @@
 __author__ = '01388863189'
 import unittest
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 from flask.ext.testing import TestCase
 from flask import Flask
 
@@ -25,9 +26,39 @@ class UserTest(TestCase):
 
         #Are we in the right page?
         self.assertIn('Apache', self.browser.title)
-        self.fail('Finish the test.')
 
+        # Is Any box to enter the username available?
+        userbox = self.browser.find_element_by_id('name')
+        self.assertEqual(userbox.get_attribute('name'), 'name')
 
+        #Time to enter the username that was previously created by an admin
+        userbox.send_keys('12345678901@test.com')
+
+        # Is there any box to enter our current password?
+        passwd = self.browser.find_element_by_id('passwd')
+        self.assertTrue(passwd)
+
+        # Time to enter our current password into the right field:
+        passwd.send_keys('Test123@enterprise')
+
+        # Is there any box to send the new password value?
+        newpwd = self.browser.find_element_by_id('newpwd')
+        self.assertTrue(newpwd)
+
+        # Time to enter our new password.
+        newpwd.send_keys('htpwd123@enterprise')
+
+        # Is there any box to confirm the new password?
+        newpwd2 = self.browser.find_element_by_id('newpwd2')
+        self.assertTrue(newpwd2)
+
+        #Time to confirm our new password and hit enter to send all changes.
+        newpwd2.send_keys('htpwd123@enterprise')
+        newpwd2.send_keys(Keys.ENTER)
+
+        # now a congratulations page will be presented with a link for the
+        # environment that uses this new credential.
+        self.assertIn('http://localhost:8000/changed', self.browser.current_url)
 
 if __name__ == '__main__':
     unittest.main()
