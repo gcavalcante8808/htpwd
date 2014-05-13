5aa7141fdcbf97049be0027469df38237fbd05c1
Gabriel Abdalla Cavalcante <gabriel.silva@serpro.gov.br>
2014-05-13 12:31:23 -0300

    Some deploy config files changed and corrected.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('requirements.txt') as file:
 
 setup(
     name='htpwd',
-    version='0.1.1',
+    version='0.1.2',
     packages=find_packages(),
     include_package_data=True,
     install_requires=required,
