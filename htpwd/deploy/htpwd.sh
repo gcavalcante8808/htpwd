79756580912dd902effe687159e0ee3c1868f555
Gabriel Abdalla Cavalcante <gabriel.silva@serpro.gov.br>
2014-05-13 12:22:09 -0300

    Logfile option and pid added.

--- htpwd/deploy/htpwd.sh
+++ htpwd/deploy/htpwd.sh
@@ -6,5 +6,5 @@ REGEXP="\d{11}@test.com"
 TARGET_PAGE=www.google.com
 
 export HTPASSWD_FILE SECRET_KEY REGEXP TARGET_PAGE
-source /data/backstage3.3/bin/activate
-gunicorn -D -c gunicorn.py htpwd.htpwd:app
+source /data/backstage/bin/activate
+gunicorn -error-logfile htpwd_error.log -p gunicorn.pid -D -c gunicorn.py htpwd.htpwd:app
