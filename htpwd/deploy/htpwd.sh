f8fc4d4c24f1cbeb59480fd7cba40ef7d66c52a7
Gabriel Abdalla Cavalcante <gabriel.silva@serpro.gov.br>
2014-05-13 12:26:10 -0300

    app should be APP in the file.

--- htpwd/deploy/htpwd.sh
+++ htpwd/deploy/htpwd.sh
@@ -7,4 +7,4 @@ TARGET_PAGE=www.google.com
 
 export HTPASSWD_FILE SECRET_KEY REGEXP TARGET_PAGE
 source /data/backstage/bin/activate
-gunicorn -error-logfile htpwd_error.log -p gunicorn.pid -D -c gunicorn.py htpwd.htpwd:app
+gunicorn -error-logfile htpwd_error.log -p gunicorn.pid -D -c gunicorn.py htpwd.htpwd:APP
