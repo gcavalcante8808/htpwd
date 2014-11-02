a6ca111f392a0e443a9702f72a0fc1110fe06be1
gcavalcante8808 <gabriel.cavalcante88@gmail.com>
2014-11-02 10:42:58 -0200

    Updated to use the new htpwd.htmanager class to serve the app.

--- htpwd/deploy/htpwd.sh
+++ htpwd/deploy/htpwd.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
 
-HTPASSWD_FILE=/data/myfile.htpasswd
-SECRET_KEY="MYKEYAAFADFADFAF"
-REGEXP="\d{11}@test.com"
-TARGET_PAGE=www.google.com
-
-export HTPASSWD_FILE SECRET_KEY REGEXP TARGET_PAGE
 source /data/backstage/bin/activate
-gunicorn -error-logfile htpwd_error.log -p gunicorn.pid -D -c gunicorn.py htpwd.htpwd:APP
+gunicorn -error-logfile htpwd_error.log -p gunicorn.pid -D -c gunicorn.py htpwd.htmanager:APP
